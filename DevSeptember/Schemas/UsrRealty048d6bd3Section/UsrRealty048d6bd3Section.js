14bfb78d8a155e8e89f426150deb026c33622e9c
OlkhovojKirill <olkhovoj.kv@pecom.ru>
2021-09-23 13:24:58 +0300

    Добавлен раздел Недвижмость

---
+++ DevSeptember/Schemas/UsrRealty048d6bd3Section/UsrRealty048d6bd3Section.js
@@ -0,0 +1,8 @@
+define("UsrRealty048d6bd3Section", [], function() {
+	return {
+		entitySchemaName: "UsrRealty",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
---
+++ DevSeptember/Schemas/UsrRealty1Page/UsrRealty1Page.js
@@ -0,0 +1,282 @@
+define("UsrRealty1Page", [], function() {
+	return {
+		entitySchemaName: "UsrRealty",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrRealtyFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrRealty"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {
+			onMyButtonClick: function() {
+				this.console.log("Hello");
+			},
+			isMyButtonEnabled: function() {
+				return true;
+			}
+		},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			// Метаданные для добавления на страницу пользовательской кнопки.
+            {
+                // Выполняется операция добавления компонента на страницу.
+                "operation": "insert",
+                // Мета-имя родительского контейнера, в который добавляется кнопка.
+                "parentName": "ProfileContainer",
+                // Кнопка добавляется в коллекцию компонентов
+                // родительского элемента.
+                "propertyName": "items",
+                // Мета-имя добавляемой кнопки.
+                "name": "MyButton",
+                // Свойства, передаваемые в конструктор компонента.
+                "values": {
+                    // Тип добавляемого элемента — кнопка.
+                    itemType: Terrasoft.ViewItemType.BUTTON,
+                    // Привязка заголовка кнопки к локализуемой строке схемы.
+                    caption: {bindTo: "Resources.Strings.MyButton"},
+                    // Привязка метода-обработчика нажатия кнопки.
+                    click: {bindTo: "onMyButtonClick"},
+                    // Привязка свойства доступности кнопки.
+                    enabled: {bindTo: "isMyButtonEnabled"},
+                    // Стиль отображения кнопки.
+                    "style": Terrasoft.controls.ButtonEnums.style.BLUE,
+					"layout": {
+						"colSpan": 24, // ширина
+						"rowSpan": 1, // высота
+						"column": 0,
+						"row": 4, // номер строки в контейнере
+						"layoutName": "ProfileContainer"
+					}
+                }
+            },
+			{
+				"operation": "insert",
+				"name": "UsrName53e4e304-5726-4bef-b59b-d9b7084a8168",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT39461908-36f2-4193-a472-34f1a3a31ced",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrPriceUSD",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "FLOATdbd876b0-ca44-4201-b942-d89c5b454b4e",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrAreaSqM",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP37430c4c-bbef-4a46-b5a0-eff0fef7500a",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "STRING25a80435-cd39-4876-b844-6a03556e4ec1",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrLocation",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPdaf63884-f63c-4138-9d3e-29c0b15eb540",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrOfferType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPa1aa7cb2-8864-4c12-8e40-40bcb861b127",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrStatus",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "STRING326429c9-28b8-45f3-931c-ace67a5dee43",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 2,
+						"column": 0,
+						"row": 2,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComment",
+					"enabled": true,
+					"contentType": 0
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": [],
+					"order": 0
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Files",
+				"values": {
+					"itemType": 2
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NotesControlGroup",
+				"values": {
+					"itemType": 15,
+					"caption": {
+						"bindTo": "Resources.Strings.NotesGroupCaption"
+					},
+					"items": []
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Notes",
+				"values": {
+					"bindTo": "UsrNotes",
+					"dataValueType": 1,
+					"contentType": 4,
+					"layout": {
+						"column": 0,
+						"row": 0,
+						"colSpan": 24
+					},
+					"labelConfig": {
+						"visible": false
+					},
+					"controlConfig": {
+						"imageLoaded": {
+							"bindTo": "insertImagesToNotes"
+						},
+						"images": {
+							"bindTo": "NotesImagesCollection"
+						}
+					}
+				},
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 1
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
